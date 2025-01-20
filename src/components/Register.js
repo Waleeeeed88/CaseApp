367fb89a661c3efde152b37f632048ec56671320
Waleeeeed88 <fifawaleed4@gmail.com>
2025-01-20 10:19:22 -0500

    Add files via upload

---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Register.css';
+
+function Register() {
+  return (
+    <div className="Register">
+        <h1> allowed to use our Service!</h1>
+    </div>
+  );
+}
+
+export default Register;
